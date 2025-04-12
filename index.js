1c5c0bffd9153478dd2ec0a7dab5d1f4a10b364c
SashaTatu <24b_tod@liceum.ztu.edu.ua>
2025-04-12 23:44:31 +0300

    Все виконано згідно 14 урока курса

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'ejs')
+
+const PORT = 3000;
+
+const createPath = (page) => path.resolve(__dirname, 'ejs', `${page}.ejs`)
+
+app.listen(PORT, 'localhost', (error) => {
+    error
+        ? console.log(error)
+        : console.log(`Сервер запущен на http://localhost:${PORT}`);
+});
+
+app.use('/styles', express.static(path.join(__dirname, 'styles')));
+
+app.get('/', (req, res) =>{
+    const title = 'Головна'
+    res.render(createPath('index'), { title })
+})
+
+app.get('/posts', (req, res) =>{
+    const title = 'Пости'
+    res.render(createPath('posts'), { title })
+})
+
+app.get('/post', (req, res) =>{
+    const title = 'Деталі посту'
+    res.render(createPath('post'), { title })
+})
+
+app.get('/add-post', (req, res) =>{
+    const title = 'Додати пост'
+    res.render(createPath('add-post'), { title })
+})
+
+app.get('/contacts', (req, res) =>{
+    const title = 'Контакти'
+    const contacts = [
+        {name: 'YouTube', link: 'https://youtube.com'},
+        {name: 'Facebook', link: 'https://facebook.com'},
+        {name: 'Instagram', link: 'https://instagram.com'}
+    ]
+    res.render(createPath('contacts'), { contacts, title })
+})
+
+app.use((req, res) =>{
+    const title = 'Помилка'
+    res
+    .status(404)
+    .render(createPath('error'), { title })
+})
