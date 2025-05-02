752a7a085fb0fafb64bbd429eaf6c8b4f898462e
SashaTatu <24b_tod@liceum.ztu.edu.ua>
2025-05-03 00:10:40 +0300

    lf

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -13,6 +14,13 @@ app.listen(PORT, '0.0.0.0', () => {
     console.log(`Сервер працює на http://localhost:${PORT}`)
   });
 
+
+  
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+
+app.use(express.urlencoded({extended: false}))
+
 app.use('/styles', express.static(path.join(__dirname, 'styles')));
 
 app.get('/', (req, res) =>{
@@ -22,17 +30,48 @@ app.get('/', (req, res) =>{
 
 app.get('/posts', (req, res) =>{
     const title = 'Пости'
-    res.render(createPath('posts'), { title })
+    const posts =[
+        {
+            id:'1',
+            text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad',
+            title:'Post title',
+            data:'02.05.2025',
+            author:'Sasha',
+        }
+    ]
+
+    res.render(createPath('posts'), { title, posts })
 })
 
-app.get('/post', (req, res) =>{
+
+app.get('/post/:id', (req, res) =>{
     const title = 'Деталі посту'
-    res.render(createPath('post'), { title })
+    const post = {
+        id:'1',
+        text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad',
+        title:'Post title',
+        data:'02.05.2025',
+        author:'Sasha',
+    }
+    res.render(createPath('post'), { title, post })
 })
+
 
 app.get('/add-post', (req, res) =>{
     const title = 'Додати пост'
     res.render(createPath('add-post'), { title })
+})
+
+app.post('/add-post', (req, res) =>{
+    const { title, author, text} = req.body
+    const post ={
+        id: new Date().getTime(),
+        data: new Date().toLocaleDateString('uk-UA'),
+        title,
+        author,
+        text
+    }
+    res.render(createPath('post'), { post, title})
 })
 
 app.get('/contacts', (req, res) =>{
